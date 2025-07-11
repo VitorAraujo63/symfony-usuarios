5c83dfb251c432975d59c09b9ba9d5ab76d63909
Vitor H. <vitor.araujo63@etec.sp.gov.br>
2025-07-10 23:14:34 -0300

    feat: create routes

---
+++ src/Controller/UsuarioController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/", name: 'web_usuario_')]
+class UsuarioController
+{
+    #[Route('/', methods: ['GET'], name: 'index')]
+    public function index(): Response
+    {
+        return new Response("implementar formulario de cadastro");
+    }
+
+    #[Route('/salvar', methods: ['POST'], name: 'salvar')]
+    public function salvar(): Response
+    {
+        return new Response("Implementar gravação ao banco de dados");
+    }
+}
