0f0466ac48d77e5cc19dc232c8dd8f62f2d7eb04
Vitor H. <vitor.araujo63@etec.sp.gov.br>
2025-07-13 14:03:42 -0300

    feat: templates with twig

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/", name: 'web_usuario_')]
-class UsuarioController
+class UsuarioController extends AbstractController
 {
     #[Route('/', methods: ['GET'], name: 'index')]
     public function index(): Response
     {
-        return new Response("implementar formulario de cadastro");
+        return $this->render("usuario/form.html.twig");
+        // return $this->render("usuario/erro.html.twig", [
+        //     'fulano' => 'Vitor'
+        // ]);
     }
 
     #[Route('/salvar', methods: ['POST'], name: 'salvar')]
